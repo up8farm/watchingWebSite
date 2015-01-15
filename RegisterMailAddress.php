9a116ee8a047e36f34119be0c33456ccad9dd6d8
up8farm <syuu104+github@gmail.com>
2015-01-16 00:17:14 +0900

    Update RegisterMailAddress.php

--- RegisterMailAddress.php
+++ RegisterMailAddress.php
@@ -5,19 +5,19 @@ require_once('func.php');
 if (isset($_POST['submit'])) {
 
     $name = htmlspecialchars($_POST['name'], ENT_QUOTES);
-    $address = htmlspecialchars($_POST['address'], ENT_QUOTES);
+    $mail = htmlspecialchars($_POST['mail'], ENT_QUOTES);
 
-    if (empty($address) && empty($name)) {
+    if (empty($mail) && empty($name)) {
         echo ' 名前とアドレスを入力して下さい<br>';
         $output_form = true;
     }
     
-    if (!empty($address) && empty($name)) {
+    if (!empty($mail) && empty($name)) {
         echo ' 名前を入力して下さい<br>';
         $output_form = true;
     }
     
-    if (empty($address) && !empty($name)) {
+    if (empty($mail) && !empty($name)) {
         echo ' アドレスを入力して下さい<br>';
         $output_form = true;
     }
@@ -26,16 +26,16 @@ if (isset($_POST['submit'])) {
     $output_form = true;
 }
 
-if (!empty($address) && !empty($name)) {
+if (!empty($mail) && !empty($name)) {
     $db = getDb();
 
     //tb_send_mailテーブルにフォームから入力された名前とメールアドレスを挿入する
-    $stmt = $db->prepare('INSERT INTO tb_send_mail (name, address) values(:name, :address)');
+    $stmt = $db->prepare('INSERT INTO tb_send_mail (name, mail) values(:name, :mail)');
     $stmt->bindValue(':name', $name, PDO::PARAM_STR);
-    $stmt->bindValue(':address', $address, PDO::PARAM_STR);
+    $stmt->bindValue(':mail', $mail, PDO::PARAM_STR);
 
     $stmt->execute();
-    echo $address . $name . "を登録しました" . "<br>";
+    echo $mail . $name . "を登録しました" . "<br>";
     echo '<a href="menu.php">メニュー画面へ</a>';
 }
 
@@ -44,7 +44,7 @@ if ($output_form) {
     <p>通知メールアドレス登録</p>
     <form method="post" action="<?php echo $_SERVER['SCRIPT_NAME']; ?>">
         名前：<input type="text" name="name"/><br>
-        メール：<input type="text" name="address"/><br>
+        メール：<input type="text" name="mail"/><br>
         <input type="submit" name="submit" value="メールアドレス登録"/>
     </form>
     <a href="menu.php">メニュー画面へ</a>
