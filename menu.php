f0ae854fdda436c914fab9d35a0f02b60ddd001f
up8farm <syuu104+github@gmail.com>
2015-01-16 01:17:11 +0900

    Update menu.php

--- menu.php
+++ menu.php
@@ -3,8 +3,8 @@
 ?>
 <h1>メニュー画面</h1>
 <ol>
-    <li><a href="RegisterMailAddress.php">メール登録</a></li>
-    <li><a href="RegisterMailAddress.php">メール登録</a></li>
+    <li><a href="RegisterMailAddress.php">通知メールアドレス登録</a></li>
+    <li><a href="DeleteMailAddress.php">通知メールアドレス削除</a></li>
     <li><a href="RegisterUrl.php">監視URL登録</a></li>
-    <li><a href="RegisterUrl.php">監視URL登録</a></li>
+    <li><a href="DeleteUrl.php">監視URL削除</a></li>
 </ol>
