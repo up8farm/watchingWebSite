76ecee96f05b22a5c1e1d857bcc25b1cb17392bf
up8farm <syuu104+github@gmail.com>
2015-01-15 19:19:03 +0900

    Update test.php

--- test.php
+++ test.php
@@ -1,13 +1,5 @@
 <?php
-/**
- * JavaScriptのalertを呼び出す
- * @param string $massage 表示するメッセージ
- */
-function callAlert($massage) {
-    echo '<script type="text/javascript">';
-    echo 'alert("' . $massage . '")';
-    echo '</script>';
-}
+require_once('func.php');
 
-callAlert("222")
+callAlert("333")
 ?>
