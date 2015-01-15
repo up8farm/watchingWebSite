ca947f52541f30bfbfbe6e2f8a8be22892064f32
up8farm <syuu104+github@gmail.com>
2015-01-16 01:55:01 +0900

    Create DeleteMailAddress.php

---
+++ DeleteMailAddress.php
@@ -0,0 +1,29 @@
+<p>通知メールアドレス削除</p>
+<form method="post" action="<?php echo $_SERVER['SCRIPT_NAME']; ?>">
+
+    <?php
+    header("Content-Type: text/html; charset=UTF-8");
+    require_once('func.php');
+
+    $db = getDb();
+
+    if (isset($_POST['submit'])) {
+    //削除するクエリ
+
+        echo '削除しました';
+    }
+
+    //一覧を表示
+    $stmt = $db->query('SELECT id, name, mail FROM tb_send_mail');
+
+    foreach ($stmt as $row) {
+        echo '<input type="checkbox" value="' . $row['id'] . '" name="todelete[]">';
+        echo $row['name'];
+        echo $row['mail'];
+        echo '<br>';
+    }
+    ?>
+
+    <input type="submit" name="submit" value="通知メールアドレス登録"/>
+</form>
+<a href="menu.php">通知メールアドレス削除</a>
