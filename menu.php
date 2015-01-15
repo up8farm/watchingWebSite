cb6b66ee6e69ab191ec78494cd4f0cb9241e515a
up8farm <syuu104+github@gmail.com>
2015-01-16 00:00:34 +0900

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,7 +1,7 @@
 <?php
     header("Content-Type: text/html; charset=UTF-8");
 ?>
-<h1>メニュー一覧</h1>
+<h1>メニュー画面</h1>
 <ol>
     <li><a href="RegisterMailAddress.php">メール登録</a></li>
     <li><a href="RegisterMailAddress.php">メール登録</a></li>
