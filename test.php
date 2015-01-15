f03dca3c4cad4e76bf5a312470742bd03247e53a
up8farm <syuu104+github@gmail.com>
2015-01-15 18:38:33 +0900

    Update test.php

--- test.php
+++ test.php
@@ -1,8 +1,13 @@
 <?php
-$mes = "aaa";
-    print<<<EOF
-    <script type="text/javascript">
-        alert("$mes");
-    </script>
-EOF;
+/**
+ * JavaScriptのalertを呼び出す
+ * @param string $massage 表示するメッセージ
+ */
+function callAlert($massage) {
+    echo '<script type="text/javascript">';
+    echo "alert('$massage')";
+    echo '</script>'
+}
+
+callAlert("222")
 ?>
