5b5b62584e3b41fabb4557a1c992c0a8441a4370
up8farm <syuu104+github@gmail.com>
2015-01-15 23:27:05 +0900

    Update RegisterMailAddress.php

--- RegisterMailAddress.php
+++ RegisterMailAddress.php
@@ -1,32 +1,49 @@
 <?php
-    require_once('func.php');
+require_once('func.php');
 
-    if (isset($_POST['submit'] and !isset($_POST['name'])){
-        callAlert('名前を入力してください');
-        die();
+if (isset($_POST['submit'])) {
+
+    $name = htmlspecialchars($_POST['name'], ENT_QUOTES);
+    $address = htmlspecialchars($_POST['address'], ENT_QUOTES);
+
+    if (empty($address) && empty($name)) {
+        echo ' 名前とアドレスを入力して下さい<br>';
+        $output_form = true;
     }
-
-    if (isset($_POST['submit'])
-        and isset($_POST['name'])
-        and isset($_POST['address'])){//メールアドレス登録ボタンが押されたら
-        
-        $name = htmlspecialchars($_POST['name'], ENT_QUOTES);
-        $address = htmlspecialchars($_POST['address'], ENT_QUOTES);
-        
-        $db = getDb();
-        
-        //tb_send_mailテーブルにフォームから入力された名前とメールアドレスを挿入する
-        $stmt = $db->prepare('INSERT INTO tb_send_mail (name, address) values(:name, :address)');
-        $stmt->bindValue(':name', $name ,PDO::PARAM_STR);
-        $stmt->bindValue(':address', $address ,PDO::PARAM_STR);
-
-        $stmt->execute();
+    
+    if (!empty($address) && empty($name)) {
+        echo ' 名前を入力して下さい<br>';
+        $output_form = true;
     }
+    
+    if (empty($address) && !empty($name)) {
+        echo ' アドレスを入力して下さい<br>';
+        $output_form = true;
+    }
+    
+} else {
+    $output_form = true;
+}
+
+if (!empty($address) && !empty($name)) {
+    $db = getDb();
+
+    //tb_send_mailテーブルにフォームから入力された名前とメールアドレスを挿入する
+    $stmt = $db->prepare('INSERT INTO tb_send_mail (name, address) values(:name, :address)');
+    $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+    $stmt->bindValue(':address', $address, PDO::PARAM_STR);
+
+    $stmt->execute();
+}
+
+if ($output_form) {
+    ?>
+    <p>通知メールアドレス登録</p>
+    <form method="post" action="<?php echo $_SERVER['SCRIPT_NAME']; ?>">
+        名前：<input type="text" name="name"/><br>
+        メール：<input type="text" name="address"/><br>
+        <input type="submit" name="submit" value="メールアドレス登録"/>
+    </form>
+    <?php
+}
 ?>
-
-<p>通知メールアドレス登録</p>
-<form method="post" action="<?php echo $_SERVER['SCRIPT_NAME']; ?>">
-    名前：<input type="text" name="name"/><br>
-    メール：<input type="text" name="address"/><br>
-    <input type="submit" name="submit" value="メールアドレス登録"/>
-</form>
