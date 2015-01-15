4eca358cc84dc0123cd000fa962d6c113aeb6e4a
up8farm <syuu104+github@gmail.com>
2015-01-16 02:20:50 +0900

    Create DeleteUrl.php

---
+++ DeleteUrl.php
@@ -0,0 +1,31 @@
+<p>監視URL削除</p>
+<form method="post" action="<?php echo $_SERVER['SCRIPT_NAME']; ?>">
+
+    <?php
+    header("Content-Type: text/html; charset=UTF-8");
+    require_once('func.php');
+
+    $db = getDb();
+
+    if (isset($_POST['submit'])) {
+        //削除するクエリ
+        foreach ($_POST['todelete'] as $delete_id) {
+            $db->query("DELETE FROM tb_crawling_url WHERE id = $delete_id");
+        }
+    }
+
+    //一覧を表示
+    $stmt = $db->query('SELECT id, title, url, hash FROM tb_crawling_url');
+
+    foreach ($stmt as $row) {
+        echo '<input type="checkbox" value="' . $row['id'] . '" name="todelete[]">';
+        echo $row['title'] . " : ";
+        echo $row['url'] . " : ";
+        echo $row['hash'];
+        echo '<br>';
+    }
+    ?>
+
+    <input type="submit" name="submit" value="監視URL削除">
+</form>
+<a href="menu.php">メニュー画面へ</a>
