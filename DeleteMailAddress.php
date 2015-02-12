1bf083968ce3f4725f1c8748e2e384a1fc48251f
up8farm <syuu104+github@gmail.com>
2015-02-12 17:09:35 +0900

    Update DeleteMailAddress.php

--- DeleteMailAddress.php
+++ DeleteMailAddress.php
@@ -7,16 +7,18 @@
 
     $db = getDb();
 
-    if (isset($_POST['submit'])) {
-        //削除するクエリ
+    if (isset($_POST['submit'])) { // 通知メールアドレス削除ボタンが押された時
+    
+        // チェックが付けられた通知メールアドレスを削除
         foreach ($_POST['todelete'] as $delete_id) {
             $db->query("DELETE FROM tb_send_mail WHERE id = $delete_id");
         }
     }
 
-    //一覧を表示
+    // 通知メールアドレス一覧を取得
     $stmt = $db->query('SELECT id, name, mail FROM tb_send_mail');
-
+    
+    // 通知メールアドレス一覧を表示
     foreach ($stmt as $row) {
         echo '<input type="checkbox" value="' . $row['id'] . '" name="todelete[]">';
         echo $row['name'] . " : ";
