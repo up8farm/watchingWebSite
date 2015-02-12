4549305206d0081e588eeea87d4ac23749d3b03a
up8farm <syuu104+github@gmail.com>
2015-02-12 16:04:54 +0900

    Update func.php

--- func.php
+++ func.php
@@ -6,8 +6,8 @@
  */
 function getDb() {
     $dsn = 'mysql:host=127.0.0.1;port=3306;dbname=db_watching_web_site;charset=utf8;';
-    $user = 'root';
-    $pass = '1qazxsw2';
+    $user = 'user';
+    $pass = 'password';
     try {
         $db = new PDO($dsn, $user, $pass);
     } catch (PDOException $e) {
